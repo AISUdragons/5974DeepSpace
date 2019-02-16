89d9a8e76a9b7c0bcedb0abedf8a9fed1d9a40eb
Carter Moss <clavierkid@gmail.com>
2019-02-16 16:38:17 -0700

    config fixes

--- src/main/java/org/usfirst/frc5974/DeepSpace/Robot.java
+++ src/main/java/org/usfirst/frc5974/DeepSpace/Robot.java
@@ -183,12 +183,12 @@ public class Robot extends TimedRobot { //https://wpilib.screenstepslive.com/s/c
 		}
 		*/
 
-		if(controls.bumperR&&targetLevel<3){
+		if(controls.runOnce(controls.bumperR,controls.pairBumperR)&&targetLevel<3){
 			//if bumper R is pressed and target level is less than 3, increase target level
 			targetLevel++;
 			System.out.println("Target++");
 	}
-	else if(controls.bumperL&&targetLevel>0){
+	else if(controls.runOnce(controls.bumperL,controls.pairBumperL)&&targetLevel>0){
 			//if bumper L is pressed and target level is more than 0, decrease target level
 			targetLevel--;
 			System.out.println("Target--");
