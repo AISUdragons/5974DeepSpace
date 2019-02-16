2bead3e1e317385d8b977a9da93de79a305f1b36
Carter Moss <clavierkid@gmail.com>
2019-02-16 16:00:21 -0700

    Changed limitLift to respond to keyboard instead of limit switches

--- src/simulator/java/frc/robot/simulator/RoboSimulator.java
+++ src/simulator/java/frc/robot/simulator/RoboSimulator.java
@@ -1,8 +1,9 @@
 package frc.robot.simulator;
 
 import com.snobot.simulator.ASimulator;
-import com.snobot.simulator.*;
-import frc.robot.Robot;
+import com.snobot.simulator.module_wrapper.TestDigitalIOJni;
+//import frc.robot.Robot;
+import org.usfirst.frc5974.DeepSpace.Robot;
 
 /**
  * A custom simulator. Currently this is empty, but if we
@@ -10,9 +11,11 @@ import frc.robot.Robot;
  * See https://github.com/pjreiniger/SnobotSimExamples/blob/master/JavaWithI2CAndSPISims/src/snobot_sim/java/com/snobot/sim/Snobot2018Simulator.java
  * for an example
  */
-public class BitBucketsSimulator extends ASimulator {
+public class RoboSimulator extends ASimulator {
 
     private Robot robot;
+
+    TestDigitalIOJni digitalTest = new TestDigitalIOJni();
 
     public void create() {
 
@@ -21,6 +24,7 @@ public class BitBucketsSimulator extends ASimulator {
     @Override
     public void update() {
         super.update();
+        digitalTest.testCreateDigitalIn();
     }
 
     public void setRobot(Robot aRobot) {
