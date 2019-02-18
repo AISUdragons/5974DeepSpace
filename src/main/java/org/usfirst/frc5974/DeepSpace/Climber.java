a887e288cc5ddf4065d9edcd61048996f2cb80ad
Carter Moss <clavierkid@gmail.com>
2019-02-18 11:30:53 -0700

    added second motor to climber (and speed controller group)

--- src/main/java/org/usfirst/frc5974/DeepSpace/Climber.java
+++ src/main/java/org/usfirst/frc5974/DeepSpace/Climber.java
@@ -1,20 +1,23 @@
 package org.usfirst.frc5974.DeepSpace;
 
 import edu.wpi.first.wpilibj.VictorSP;
+import edu.wpi.first.wpilibj.SpeedControllerGroup;
 
 import org.usfirst.frc5974.DeepSpace.Controller;
 
 public class Climber{
-    VictorSP motorClimb = new VictorSP(7);
+    VictorSP motorClimbL = new VictorSP(7);
+    VictorSP motorClimbR = new VictorSP(8);
+    SpeedControllerGroup motorsClimb = new SpeedControllerGroup(motorClimbL,motorClimbR);
     Controller controls = new Controller();
     double climbSpeed = 1;
 
     public void climb(){
         if(controls.buttonStart){
-            motorClimb.set(climbSpeed);
+            motorsClimb.set(climbSpeed);
         }
         else if(!controls.buttonStart){
-            motorClimb.set(0);
+            motorsClimb.set(0);
         }
     }
 }
