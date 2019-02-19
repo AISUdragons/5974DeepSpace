8f4d4215daaac74f513ac93582a4fcc46ff91c52
Laptop B4 <mechanical.dragons@aisutah.org>
2019-02-18 21:06:39 -0700

    Unhandled exception instantiating robot edu.wpi.first.hal.DIOJNI edu.wpi.first.hal.util.UncleanStatusException: Code: -1029. HAL: Resource already allocated

--- src/main/java/org/usfirst/frc5974/DeepSpace/Sucker.java
+++ src/main/java/org/usfirst/frc5974/DeepSpace/Sucker.java
@@ -8,37 +8,43 @@ import org.usfirst.frc5974.DeepSpace.Controller;
 public class Sucker{
     Controller controls = new Controller();
 
-    DigitalInput switchSucker = new DigitalInput(5);
-    DigitalInput switchBottom = new DigitalInput(6);
+    //DigitalInput switchSucker = new DigitalInput(5);
+    //DigitalInput switchBottom = new DigitalInput(6);
 
     //These will have 2 motor controllers each.
+    /*
     VictorSP motorsSuckerBase = new VictorSP(7); 
     VictorSP motorsSuckerSpinner = new VictorSP(8);
 
     double pivotSpeed;
     double suckSpeed=.5;
     double climbSpeed=.75;
+    */
 
     public void succ(){
-        motorsSuckerSpinner.set(suckSpeed);
+        //motorsSuckerSpinner.set(suckSpeed);
     }
 
     public void setup(){
+        /*
         motorsSuckerBase.set(pivotSpeed);
-        if(switchSucker.get()){
+        //if(switchSucker.get()){
             motorsSuckerBase.set(0);
-        }
+        //}
+        */
     }
 
     public void climb(){
+        /*
         if(controls.buttonX){
-            if(switchBottom.get()){
+            //if(switchBottom.get()){
                 motorsSuckerBase.set(0);
-            }
-            else{
+            //}
+            //else{
                 motorsSuckerBase.set(pivotSpeed);
-            }
+            //}
             motorsSuckerSpinner.set(climbSpeed);
         }
+        */
     }
 }
